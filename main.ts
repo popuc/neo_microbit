9523f494c9bba02322b7b97604840711a98e125f
POP <40453939+popuc@users.noreply.github.com>
2021-03-26 18:24:32 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,13 @@ input.onButtonPressed(Button.A, function () {
         # . . # #
         # . . . #
         `)
+    basic.showLeds(`
+        . # # # .
+        . . # # .
+        . # . # .
+        . # # . .
+        . # # # .
+        `)
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showLeds(`
@@ -15,13 +22,27 @@ input.onButtonPressed(Button.AB, function () {
         # . . . #
         # # # # #
         `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
-        # # # # #
+        # # # . .
         # . . . .
-        # # # # #
+        # # # . .
         # . . . .
-        # # # # #
+        # # # . .
+        `)
+    basic.showLeds(`
+        . . . # #
+        . # # # #
+        . . . # #
+        . # # # #
+        . . . # #
         `)
 })
